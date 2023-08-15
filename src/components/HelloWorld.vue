8fb6d1fc825fb5e0187a54ef0b296b6b51c180a8
linnk-1 <1398447668@qq.com>
2023-08-15 17:04:48 +0800

    vue2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,7 +27,11 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <div>
+    <img src="" alt="">
   </div>
+  </div>
+  
 </template>
 
 <script>
